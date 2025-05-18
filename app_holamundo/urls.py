4141f916bb48ac639073b9ceeee579b615c83e00
Bryan Muñoz <tiagotroller021019@gmail.com>
2025-05-18 11:36:51 -0500

    Se agregaron estilos y se corrigieron rutas de imágenes

--- app_holamundo/urls.py
+++ app_holamundo/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("pages.urls")),
+    path('', include('pages.urls')),
 ]
 
 # Servir archivos estáticos en desarrollo
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,6 @@
-# pages/urls.py
 from django.urls import path
 from .views import homePageView
 
 urlpatterns = [
-    path('', homePageView, name="home"),
+    path('', homePageView, name='home'),
 ]
